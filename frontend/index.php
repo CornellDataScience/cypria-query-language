86f60fc0df194cf99b092077bc2af9a8071aadf2
David Kim <jk2537@cornell.edu>
2020-05-06 17:39:04 -0500

    Website frontend mostly implemented

--- frontend/index.php
+++ frontend/index.php
@@ -19,6 +19,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
   <script type="text/javascript" src="scripts/main.js"></script>
+  <!-- Fonts -->
+  <link rel="stylesheet" type="text/css" href="fonts/roboto-normal.css">
+  <link rel="stylesheet" type="text/css" href="fonts/roboto-light.css">
+  <link rel="stylesheet" type="text/css" href="fonts/roboto-bold.css">
   <!-- Keywords, names, properties go here later -->
 </head>
 
@@ -33,7 +37,62 @@
 
   <!-- Content -->
   <div class="main-container">
-    <h1 style="text-align: center">Hello</h1>
+    <div class="main">
+      <div class="intro section">
+        <div id="intro-description">
+          Cypria is an open-source query language that interprets to SQL,
+          making it easy to build complicated queries with
+          <span>functional</span> constructs.
+        </div>
+        <div id="intro-image"></div>
+        <div id="intro-download">Download Cypria (coming soon)</div>
+      </div>
+      <div class="playground section">
+        <h1>Try Cypria</h1>
+        <div id="code-box">
+          <textarea>
+let tbl = "products" in
+tbl |> filter_max ["name";"price"] "price"&#13;&#10;</textarea>
+        </div>
+        <div id="code-output">
+          <pre><span class="output">SELECT name, MAX(price) as price FROM products</span></pre>
+        </div>
+        <button id="run-button">Run code</button>
+      </div>
+      <div class="links section">
+        <h1>Documentation & links</h1>
+        <ul>
+          <li>
+            <a href="https://github.com/CornellDataScience/cypria-query-language/blob/master/cypria_planning/language_spec.md">
+              Cypria documentation
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/CornellDataScience/cypria-query-language">
+              GitHub repository
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/CornellDataScience/cypria-query-language">
+              Foo
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/CornellDataScience/cypria-query-language">
+              Bar
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/CornellDataScience/cypria-query-language">
+              Baz
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="other section">
+        <h1>Reserved space</h1>
+      </div>
+    </div>
 
     <!-- Footer -->
     <footer>
