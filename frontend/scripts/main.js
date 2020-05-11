f0b8523458c680f6af05d2bc2310b44fd12aa08b
David Kim <jk2537@cornell.edu>
2020-05-11 15:46:20 -0500

    Trying again after some minor fixes

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -7,21 +7,14 @@ $(document).ready(function () {
     $.ajax({
       url: "http://lang.cypriaql.com/cypria",
       // url: "https://reqres.in/api/users/2",
-      type: "GET",
+      type: "POST",
       data: {
         cypria_raw: $code
       },
       dataType: "json",
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With"
-      },
-      crossDomain: true,
-      beforeSend: function (xhr) {
-        xhr.withCredentials = true;
-      },
+        "Content-Type": "application/json"
+      }
     }).done(function (res) {
       $("pre").html(`<code>${JSON.stringify(res, undefined, 2)}</code>`);
     }).fail(function (jqXHR, err) {
